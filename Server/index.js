278ce7b94fce0b9cb0b0de01928ba72ef7b9d107
Betelhem-Belete <betelhembelete0@gmail.com>
2024-02-01 15:34:16 -0500

    dbcon

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,15 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(body-parser.json({ limit: "30mb", extended: true}));
-app.use(body-parser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyparser.json({ limit: "30mb", extended: true}));
+app.use(bodyparser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+const DB_URL = 'mongodb+srv://uninews:uninews@cluster0.qp0nwzv.mongodb.net/'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(DB_URL)
+    .then( () => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
