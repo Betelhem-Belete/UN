b166f650935b1f73f16338ae2c05ff7c83b1ce4b
Betelhem <119102991+Betelhem-Belete@users.noreply.github.com>
2024-02-06 16:36:02 -0500

    Update index.jsx

--- Client/src/api/index.jsx
+++ Client/src/api/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 //const url = 'http://127.0.0.1:5000/news';
-const url = 'unews-snowy.vercel.app/news';
+const url = 'https://unews-snowy.vercel.app/news';
 
 export const fetchNews = () => axios.get(url);
 export const createNew = (newNew) => axios.post(url, newNew);
